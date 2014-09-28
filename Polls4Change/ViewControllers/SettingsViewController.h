1b4b939af0ae7279ac9d430861e720e570f4b927
MarcoRmz <marcormz19@gmail.com>
2014-09-28 01:10:21 -0500

    added Settings view & changed UI

---
+++ Polls4Change/ViewControllers/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  Polls4Change
+//
+//  Created by Marco Ramirez on 9/28/14.
+//  Copyright (c) 2014 P4C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
