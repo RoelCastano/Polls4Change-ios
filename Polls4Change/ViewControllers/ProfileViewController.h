aaef2a7a761ca8b211a24770a133c98f4bb98810
MarcoRmz <marcormz19@gmail.com>
2014-09-28 02:12:03 -0500

    changes to Profile UI

---
+++ Polls4Change/ViewControllers/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  Polls4Change
+//
+//  Created by Marco Ramirez on 9/28/14.
+//  Copyright (c) 2014 P4C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
